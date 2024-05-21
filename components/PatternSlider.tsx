fa56a8ff61a7ee5cb537f621dd52873f77c4d98e
Muhammad Jahad Tariq <82119480+jahadtariq@users.noreply.github.com>
2024-05-21 07:38:21 +0500

    Update PatternSlider.tsx

--- components/PatternSlider.tsx
+++ components/PatternSlider.tsx
@@ -4,90 +4,90 @@ const PatternSlider = () => {
     <div className='mt-2 bg-[#111111] w-full h-[104px] inline-flex flex-nowrap items-center shadow-[#000000E5] shadow-3xl overflow-hidden '>
         <ul className='flex items-center justify-center md:justify-start [&_li]:-mx-5 [&_img]:max-w-none animate-infinite-scroll'>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
         </ul>
         <ul className='flex items-center justify-center md:justify-start [&_li]:-mx-5 [&_img]:max-w-none animate-infinite-scroll' aria-hidden="true">
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
             <li>
-                <img src="../src/assets/pattern.svg"/>
+                <img src="/pattern.svg"/>
             </li>
         </ul>
     </div>
